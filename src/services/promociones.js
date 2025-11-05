646a16d1083dceef2904b6161349756a9f54050e
¨susy215¨ <yandiravivancos@gmail.com>
2025-11-04 23:53:25 -0400

    validar

--- src/services/promociones.js
+++ src/services/promociones.js
@@ -8,7 +8,7 @@ export async function getPromociones(vigentes = true) {
 
 export async function validarPromocion(codigo, monto) {
   // Según la documentación del backend, el endpoint es /validar_codigo/ con params
-  const { data } = await api.get('/promociones/promociones/validar_codigo/', {
+  const { data } = await api.get('/promociones/promociones/validar/', {
     params: {
       codigo: codigo.toUpperCase(),
       monto
